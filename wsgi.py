4e7d266e28a6cb391475b63d1990cacb7f5a94df
Hassaan Riaz <hassaan.riaz@mezino.com>
2017-04-11 13:31:50 +0500

    wsgi file updated

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moirsintl.settings")
+"""
+WSGI config for moirs project.
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moirs.settings")
+
+application = get_wsgi_application()
